fa370b32d840c35105ec1a4446fac30b1523999d
jose luis <cortezluisjose@gmail.com>
2019-12-13 06:30:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 require './config/environment'
-
+require './app/models/tweet'
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+   get '/' do
+    @tweets = Tweet.all
+    erb :index 
   end
 
 end
